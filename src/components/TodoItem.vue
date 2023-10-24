4d5f494dac0f0053115cb1756380bd5563189f1b
daniel j <djun44@gmail.com>
2023-10-24 11:14:56 -0300

    add completed push, edit put task

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -4,7 +4,12 @@
 border-gray-400 last:border-b-0">
             <div class="flex items-center justify-center 
 mr-2">
-                <button class="text-gray-400">
+                <button :class="{
+                    'text-green-500': isCompleted,
+                    'text-gray-400': !isCompleted,
+                }"
+                    @click="onCheckClick"
+                >
                     <svg class="w-5 h-5" fill="none" 
 stroke="currentColor" viewBox="0 0 24 24" 
 xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" 
@@ -14,9 +19,10 @@ stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>
 
             <div class="w-full">
                 <input
+                    v-model="title"
                     type="text"
                     placeholder="Digite a sua tarefa"
-                    :value="todo.title"
+                    @keyup.enter="onTitleChange"
                     class="bg-gray-300 placeholder-gray-500 
 text-gray-700 font-light focus:outline-none block w-full appearance-none 
 leading-normal mr-3"
@@ -56,6 +62,37 @@ justify-center">
                 type: Object,
                 default: () => ({}),
             }
-        }
+        },
+        data() {
+            return {
+                title: this.todo.title,
+                isCompleted: this.todo.completed,
+            }
+        },
+
+        methods: {
+            onTitleChange() {
+                
+                if (!this.title) {
+                    return;
+                }
+                this.updateTodo()
+            },
+            updateTodo() {
+                
+                const payload = {
+                    id: this.todo.id,
+                    data: {
+                        title: this.title,
+                        completed: this.isCompleted
+                    }
+                }
+                this.$store.dispatch('updateTodo', payload)
+            },
+            onCheckClick() {
+                this.isCompleted = !this.isCompleted
+                this.updateTodo()
+            }
+        } 
     }
 </script>
